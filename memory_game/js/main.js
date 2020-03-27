1b2fd4ddaf7c330af83804e65889283475927946
Ethan <ashethank@gmail.com>
2020-03-27 13:59:23 -0500

    added js, created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+console.log("up and running");
+let cardOne = "Queen";
+let cardTwo = "Queen";
+let cardThree = "King";
+let cardFour = "King";
+
+console.log("User Flipped "+cardOne);
+console.log("User Flipped "+cardThree);
