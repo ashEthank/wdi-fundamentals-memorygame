f4fffd38eb54f2ecb54336687b33eb5ab565aebe
Ethan <ashethank@gmail.com>
2020-04-05 02:34:57 -0500

    added js objects, and game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 console.log("up and running");
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+];
+
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -8,11 +30,13 @@ function checkForMatch() {
   console.log("You found a match!");
 } else {
   console.log("Sorry, try again.");
+  alert("Sorry, try again.")
 }
-}
+};
 
 function flipCard(cardId) {	
-	console.log("User Flipped "+cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-}
-
+	console.log("User Flipped "+cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+};
